a26ee544b5916de0828511840b81d85c6137ff8f
JLP0578 <j.lepivert@outlook.fr>
2023-10-24 16:32:43 +0200

    [Init] App/ : structure

---
+++ App/Class/NomDepartement.py
@@ -0,0 +1,22 @@
+class NomDepartement:
+    def __init__(self, nom, code):
+        self.nom = nom
+        self.code = code
+
+    def methode(self):
+        print("Ceci est une méthode de la classe.")
+
+
+
+
+
+
+# # Création d'une instance de la classe
+# objet = NomDepartement("Valeur1", "Valeur2")
+
+# # Utilisation des attributs de l'objet
+# print(objet.attribut1)
+# print(objet.attribut2)
+
+# # Appel d'une méthode de l'objet
+# objet.methode()
